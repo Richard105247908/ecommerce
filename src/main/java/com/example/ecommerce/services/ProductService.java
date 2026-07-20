8f734b6fa77ff2d439585f67724bcefd80c7e597
Richard <rj.n@live.com>
2026-07-20 13:06:33 +0100

    product service methods for id

--- src/main/java/com/example/ecommerce/services/ProductService.java
+++ src/main/java/com/example/ecommerce/services/ProductService.java
@@ -1,4 +1,29 @@
 package com.example.ecommerce.services;
 
+import com.example.ecommerce.models.Product;
+import com.example.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product getProductById(long id) {
+        Optional<Product> optional = productRepository.findById(id);
+
+        Product product = null;
+
+        if (optional.isPresent()) {
+            product = optional.get();
+        } else {
+            throw new RuntimeException("Product not found");
+        }
+        return product;
+    }
+    
 }
