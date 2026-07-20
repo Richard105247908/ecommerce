d3a0e9b7dfc92011e4723974db2fc2ca81055461
Richard <rj.n@live.com>
2026-07-20 14:00:25 +0100

    .

--- src/main/java/com/example/ecommerce/services/UserService.java
+++ src/main/java/com/example/ecommerce/services/UserService.java
@@ -1,20 +1,32 @@
 package com.example.ecommerce.services;
 
+import com.example.ecommerce.models.Users;
 import com.example.ecommerce.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    private final Map<String, Users> users=new HashMap<>();
     private final PasswordEncoder passwordEncoder;
 
     public UserService(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
 
-        
+        //Adding a default admin user
+        Users admin=new Users();
+        admin.setUsername("admin");
+        admin.setPassword(passwordEncoder.encode("admin123"));
+        admin.setRole("ADMIN");
+        users.put(admin.getUsername(), admin);
+
+        //
     }
 }
