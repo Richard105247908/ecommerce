51e6bb09bec7f73450b4eff769ca0f449bed5cdf
Richard <rj.n@live.com>
2026-07-20 13:16:41 +0100

    methods for crud

--- src/main/java/com/example/ecommerce/services/ProductService.java
+++ src/main/java/com/example/ecommerce/services/ProductService.java
@@ -5,6 +5,7 @@ import com.example.ecommerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,8 +14,17 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    public List<Product> getAllProducts(){
+        return this.productRepository.findAll();
+    }
+
+    public Product addProduct(Product product){
+        return this.productRepository.save(product);
+    }
+
+
     public Product getProductById(long id) {
-        Optional<Product> optional = productRepository.findById(id);
+        Optional<Product> optional = this.productRepository.findById(id);
 
         Product product = null;
 
@@ -25,5 +35,9 @@ public class ProductService {
         }
         return product;
     }
+
     
+
+
+
 }
