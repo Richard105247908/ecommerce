a905c4e9157a811e856a7d1c4b24145a971477d1
Richard <rj.n@live.com>
2026-07-20 15:32:20 +0100

    security config

--- src/main/java/com/example/ecommerce/SecurityConfig.java
+++ src/main/java/com/example/ecommerce/SecurityConfig.java
@@ -1,4 +1,65 @@
 package com.example.ecommerce;
 
+import com.example.ecommerce.models.Users;
+import com.example.ecommerce.services.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
-}
+
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService){
+        this.userService=userService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/register", "/login").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .requestMatchers("/products/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf().disable()
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/welcome", true)
+                )
+                // 5. Configure Logout
+                .logout(logout -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            com.example.ecommerce.models.Users user = userService.findByUsername(username);
+            if (user == null) {
+                throw new UsernameNotFoundException("User not found");
+            }
+            User.UserBuilder builder = User.withUsername(user.getUsername());
+            builder.password(user.getPassword());  // Password is already encoded
+            if(user.getRole() != null) {
+                builder.roles(user.getRole());
+            }
+            return builder.build();
+        };
+
+
+        }
+
+
+    }
+
