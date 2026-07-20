c0f1adba99c4413eac6120e305c97db01ebfd066
Richard <rj.n@live.com>
2026-07-20 13:55:32 +0100

    user servuice

--- src/main/java/com/example/ecommerce/services/UserService.java
+++ src/main/java/com/example/ecommerce/services/UserService.java
@@ -1,4 +1,12 @@
 package com.example.ecommerce.services;
 
+import com.example.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
 }
