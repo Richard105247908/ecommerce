1762a8befe8e41da7afc4d5773d4766e0af0febc
Richard <rj.n@live.com>
2026-07-20 13:56:51 +0100

    password encoder

--- src/main/java/com/example/ecommerce/services/UserService.java
+++ src/main/java/com/example/ecommerce/services/UserService.java
@@ -2,6 +2,7 @@ package com.example.ecommerce.services;
 
 import com.example.ecommerce.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +10,11 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+
+        
+    }
 }
