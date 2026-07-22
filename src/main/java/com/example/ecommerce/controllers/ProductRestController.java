2181da4a64603fd76cd056def8949366f2a6b8d9
Richard <rj.n@live.com>
2026-07-22 10:10:17 +0100

    inject product service into product controller

--- src/main/java/com/example/ecommerce/controllers/ProductRestController.java
+++ src/main/java/com/example/ecommerce/controllers/ProductRestController.java
@@ -1,4 +1,18 @@
 package com.example.ecommerce.controllers;
 
+import com.example.ecommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products/api")
+
 public class ProductRestController {
+
+    @Autowired
+    private ProductService productService;
+
+    
+
 }
