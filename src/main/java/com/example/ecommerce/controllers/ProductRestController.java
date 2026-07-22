5c12bba0d27e15416e2f80128af66d2330bca982
Richard <rj.n@live.com>
2026-07-22 10:59:47 +0100

    edits made

--- src/main/java/com/example/ecommerce/controllers/ProductRestController.java
+++ src/main/java/com/example/ecommerce/controllers/ProductRestController.java
@@ -25,7 +25,7 @@ public class ProductRestController {
     }
 
     @PostMapping
-    public Product saveProducts(@PathVariable Product product){
+    public Product saveProducts(@RequestBody Product product){
       return  productService.addProduct(product);
     }
 
@@ -35,7 +35,7 @@ public class ProductRestController {
     }
 
     @PutMapping("/{id}")
-    public Product updateProduct(@PathVariable long id, Product updateProduct){
+    public Product updateProduct(@PathVariable long id, @RequestBody Product updateProduct){
         return productService.updateProduct(id, updateProduct);
     }
 
